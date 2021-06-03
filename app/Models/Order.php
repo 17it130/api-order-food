b4ea13d519b83784e44ff73311972545747fff1f
nqcuong.dev <nqcuong.dev@gmail.com>
2021-06-04 04:17:18 +0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -20,6 +20,6 @@ class Order extends Model
     }
 
     public function notification() {
-        return $this->belongsTo(Notification::class, 'order_id');
+        return $this->belongsTo(Notification::class, 'order_id', 'id');
     }
 }
